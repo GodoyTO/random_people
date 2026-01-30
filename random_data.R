f079eeaa48bf092db623febe5362047c81bbb459
GodoyTO <tgodoyoliveira@gmail.com>
2026-01-29 21:09:40 -0300

    feat: refactoring regex to change CamelCase into snake_case

--- random_data.R
+++ random_data.R
@@ -3,23 +3,30 @@ if(!require(tidyverse)) install.packages('tidyverse'); require(tidyverse)
 ds_original <- read_csv("FakeNameGenerator.com_f469bbf6.csv")
 
 ds <- ds_original %>% 
-    rename_with(~str_to_lower(str_replace_all(., "(?<=.)([A-Z])", "_\\1"))) %>% 
-    select(cd_pac = number, tp_sex = gender, nm_ethn = name_set, nm_first = given_name,
-           nm_middle = mothers_maiden, nm_last = surname,
-           cd_cpf = national_i_d, dt_birth = birthday, nu_age = age, nu_weight = kilograms,
-           nu_height = centimeters,
+    rename_with(~str_to_lower(str_replace_all(., "([a-z])([A-Z])", "\\1_\\2"))) %>% 
+    select(cd_pac = number, tp_sex = gender, nm_ethn = name_set,
+           nm_first = given_name, nm_middle = mothers_maiden, nm_last = surname,
+           cd_cpf = national_id, dt_birth = birthday, nu_age = age,
+           nu_weight = kilograms, nu_height = centimeters,
            nm_city = city, cd_state = state, lat = latitude, long = longitude,
            nm_sign = tropical_zodiac, nm_color = color, tp_blood = blood_type,
-           vehicle, occupation,
-           nm_cc = c_c_type, nu_cc = c_c_number, cd_cc = c_v_v2, dt_cc_exp = c_c_expires,
-           email_address, username, password, guid = g_u_i_d
+           vehicle, occupation, nm_cc = cctype, nu_cc = ccnumber,
+           cd_cc = cvv2, dt_cc_exp = ccexpires,
+           email_address, username, password, guid
            )
 
 ds <- ds %>% 
-    mutate(cd_pac   = sprintf("%06d", cd_pac),
-           tp_sex   = if_else(tp_sex  == 'female', 'F', 'M'),
-           nm_ethn  = if_else(nm_ethn == 'Japanese (Anglicized)', 'Japanese', nm_ethn),
-           dt_birth = mdy(dt_birth))
+    mutate(
+        cd_pac   = sprintf("%06d", cd_pac),
+        tp_sex   = if_else(tp_sex  == 'female', 'F', 'M'),
+        nm_ethn  = if_else(nm_ethn == 'Japanese (Anglicized)', 'Japanese', nm_ethn),
+        dt_birth = mdy(dt_birth),
+        nu_cc    = as.character(nu_cc)
+    )
+
+ds <- ds %>% 
+    mutate(nm_full = paste(nm_first, nm_middle, nm_last), .after = tp_sex) %>% 
+    select(-c(nm_first, nm_middle, nm_last))
 
 ds <- ds %>% 
     mutate(
@@ -30,3 +37,4 @@ ds <- ds %>%
             T ~ cd_cpf
         )
     )
+
